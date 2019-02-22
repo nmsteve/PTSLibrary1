1e1e72b8168eb7b0f7de599abb21b75f43681679
Stephen Mwaura <nmsteve45@gmail.com>
2019-02-22 20:55:07 +0300

    Add project files.

---
+++ PTSLibrary/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary
+{
+    public enum Status
+    {
+        ReadyToSTart = 1,
+        InProgress = 2,
+        Completed = 3,
+        WaitingInProgress = 4
+    }
+}
